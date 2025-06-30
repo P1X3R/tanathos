fa2ede9d89e4b33c464f25df53b8dfd20a0a15b1
P1X3R <p1x3r@proton.me>
2025-06-30 11:45:45 -0500

    Board representation

---
+++ include/board.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "sysifus.h"
+#include <array>
+#include <cstdint>
+#include <functional>
+#include <numeric>
+
+struct ChessBoard {
+  std::array<uint64_t, KING + 1> whites, blacks;
+  uint64_t zobrist;
+  uint32_t halfmoveCounter : 7, enPassantSquare : 6; // 0 means "no en passant"
+  uint32_t whiteToMove : 1;
+  union {
+    struct {
+      uint32_t whiteKingSide : 1, whiteQueenSide : 1, blackKingSide : 1,
+          blackQueenSide : 1;
+    };
+    uint32_t compressed : 4;
+  } castlingRights;
+
+  auto getFlat(const bool forWhites) -> uint64_t {
+    const std::array<uint64_t, KING + 1> &color =
+        forWhites ? this->whites : this->blacks;
+
+    return std::reduce(color.begin(), color.end(), 0, std::bit_or{});
+  }
+};
