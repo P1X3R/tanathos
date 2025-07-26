eef8dc64beb0a3116abacfd611f51f743b176c9e
P1X3R <p1x3r@proton.me>
2025-07-26 11:32:54 -0500

    Generate more robust Zobrist tables

--- src/zobrist.cpp
+++ src/zobrist.cpp
@@ -1,33 +1,32 @@
 #include "bitboard.h"
 #include "sysifus.h"
-#include <chrono>
+#include <array>
 #include <cstdint>
 #include <random>
 
-static std::array<std::uint64_t, BOARD_AREA> ZOBRIST_SQUARE;
-static std::array<std::uint64_t, Piece::KING + 1> ZOBRIST_PIECE;
-static std::array<std::uint64_t, 2> ZOBRIST_TURN;
+extern std::array<
+    std::array<std::array<std::uint64_t, BOARD_AREA>, Piece::KING + 1>, 2>
+    ZOBRIST_PIECE;
+static std::uint64_t ZOBRIST_TURN;
 static std::array<std::uint64_t, 1 << 4> ZOBRIST_CASTLING_RIGHTS;
 static std::array<std::uint64_t, BOARD_LENGTH> ZOBRIST_EN_PASSANT_FILE;
 
 struct ZobristInitializer {
   ZobristInitializer() {
     std::random_device device;
-    std::minstd_rand gen(
-        std::chrono::high_resolution_clock::now().time_since_epoch().count());
+    std::seed_seq seed{device(), device(), device(), device()};
+    std::mt19937_64 gen(seed);
     std::uniform_int_distribution<std::uint64_t> distribution;
 
-    for (std::uint64_t &zobrist : ZOBRIST_SQUARE) {
-      zobrist = distribution(gen);
+    for (auto &color : ZOBRIST_PIECE) {
+      for (auto &pieceType : color) {
+        for (std::uint64_t &zobrist : pieceType) {
+          zobrist = distribution(gen);
+        }
+      }
     }
 
-    for (std::uint64_t &zobrist : ZOBRIST_PIECE) {
-      zobrist = distribution(gen);
-    }
-
-    for (std::uint64_t &zobrist : ZOBRIST_TURN) {
-      zobrist = distribution(gen);
-    }
+    ZOBRIST_TURN = distribution(gen);
 
     for (std::uint64_t &zobrist : ZOBRIST_CASTLING_RIGHTS) {
       zobrist = distribution(gen);
