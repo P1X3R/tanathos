2fecaf5b56fe93e39b5dd4a9e0b83a8e2b61f53d
P1X3R <p1x3r@proton.me>
2025-07-26 15:39:37 -0500

    Struct that stores context to undo move

--- include/move.h
+++ include/move.h
@@ -3,5 +3,13 @@
 #include "board.h"
 #include "legalMoves.h"
 
+struct UndoCTX {
+  MoveCTX move;
+  CastlingRights castlingRights;
+  std::uint32_t halfmoveClock : 7;
+  std::uint32_t enPassantSquare : 6; // 0 means no en passant
+  std::uint64_t zobrist;
+};
+
 void makeMove(ChessBoard &board, const MoveCTX &ctx);
-void undoMove(ChessBoard &board, const MoveCTX &lastMove);
+void undoMove(ChessBoard &board, const UndoCTX &ctx);
