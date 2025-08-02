0c376d2a6276734b3adb7601cee2fac4a57b476e
P1X3R <p1x3r@proton.me>
2025-08-02 14:40:10 -0500

    Refactor: Enhance Perft function to return detailed statistics

--- test/perft.cpp
+++ test/perft.cpp
@@ -1,36 +1,32 @@
 #include "board.h"
 #include "legalMoves.h"
 #include "move.h"
+#include "sysifus.h"
 #include "gtest/gtest.h"
 #include <cstdint>
 #include <string>
 
+struct PerftResult {
+  std::uint64_t nodes = 0;
+  std::uint64_t captures = 0;
+  std::uint64_t promotions = 0;
+  std::uint64_t checks = 0;
+};
+
 class PerftTest : public ::testing::Test {
 protected:
-  auto perft(std::uint8_t depth, ChessBoard &board, const std::uint64_t flat)
-      -> std::uint64_t {
-    if (depth == 0) {
-      return 1ULL;
-    }
-
-    MoveGenerator whitesGenerator;
-    MoveGenerator blacksGenerator;
-
-    whitesGenerator.generatePseudoLegal(board, true);
-    blacksGenerator.generatePseudoLegal(board, false);
-
-    UndoCTX undo = {
-        .move = {},
-        .castlingRights = board.castlingRights,
-        .halfmoveClock = board.halfmoveClock,
-        .enPassantSquare = board.enPassantSquare,
-        .zobrist = board.zobrist,
-    };
-
+  static auto singlePerft(ChessBoard &board) {
+    PerftResult result;
     const bool forWhites = board.whiteToMove;
 
+    MoveGenerator whitesGenerator;
+    whitesGenerator.generatePseudoLegal(board, true);
+    MoveGenerator blacksGenerator;
+    blacksGenerator.generatePseudoLegal(board, false);
+
     const CastlingRights castlingAttackMask = generateCastlingAttackMask(
-        flat, whitesGenerator.kills, blacksGenerator.kills);
+        board.getFlat(true) | board.getFlat(false), whitesGenerator.kills,
+        blacksGenerator.kills);
 
     if (forWhites) {
       whitesGenerator.appendCastling(board, castlingAttackMask, true);
@@ -38,24 +34,91 @@ protected:
       blacksGenerator.appendCastling(board, castlingAttackMask, false);
     }
 
-    std::uint64_t nodes = 0;
-
     MoveGenerator &generator = forWhites ? whitesGenerator : blacksGenerator;
 
-    for (MoveCTX &move : generator.pseudoLegal) {
-      undo.move = move;
+    for (const auto &move : generator.pseudoLegal) {
+      UndoCTX undo = {
+          .move = move,
+          .castlingRights = board.castlingRights,
+          .halfmoveClock = board.halfmoveClock,
+          .enPassantSquare = board.enPassantSquare,
+          .zobrist = board.zobrist,
+      };
 
       makeMove(board, move);
 
       if (!board.isKingInCheck(forWhites)) {
-        nodes +=
-            perft(depth - 1, board, board.getFlat(true) | board.getFlat(false));
+        result.nodes++;
+        if (move.captured != Piece::NOTHING) {
+          result.captures++;
+        }
+        if (move.promotion != Piece::NOTHING) {
+          result.promotions++;
+        }
+        if (board.isKingInCheck(!forWhites)) {
+          result.checks++;
+        }
       }
 
       undoMove(board, undo);
     }
 
-    return nodes;
+    return result;
+  }
+
+  auto perft(const std::uint8_t depth, ChessBoard &board) -> PerftResult {
+    if (depth == 0) {
+      return {.nodes = 1, .captures = 0, .promotions = 0, .checks = 0};
+    }
+
+    if (depth == 1) {
+      return singlePerft(board);
+    }
+
+    PerftResult totalResult;
+
+    const bool forWhites = board.whiteToMove;
+
+    MoveGenerator whitesGenerator;
+    whitesGenerator.generatePseudoLegal(board, true);
+    MoveGenerator blacksGenerator;
+    blacksGenerator.generatePseudoLegal(board, false);
+
+    const CastlingRights castlingAttackMask = generateCastlingAttackMask(
+        board.getFlat(true) | board.getFlat(false), whitesGenerator.kills,
+        blacksGenerator.kills);
+
+    if (forWhites) {
+      whitesGenerator.appendCastling(board, castlingAttackMask, true);
+    } else {
+      blacksGenerator.appendCastling(board, castlingAttackMask, false);
+    }
+
+    MoveGenerator &generator = forWhites ? whitesGenerator : blacksGenerator;
+
+    for (const auto &move : generator.pseudoLegal) {
+      UndoCTX undo = {
+          .move = move,
+          .castlingRights = board.castlingRights,
+          .halfmoveClock = board.halfmoveClock,
+          .enPassantSquare = board.enPassantSquare,
+          .zobrist = board.zobrist,
+      };
+
+      makeMove(board, move);
+
+      if (!board.isKingInCheck(forWhites)) {
+        PerftResult subResult = perft(depth - 1, board);
+        totalResult.nodes += subResult.nodes;
+        totalResult.captures += subResult.captures;
+        totalResult.promotions += subResult.promotions;
+        totalResult.checks += subResult.checks;
+      }
+
+      undoMove(board, undo);
+    }
+
+    return totalResult;
   }
 };
 
@@ -64,7 +127,15 @@ TEST_F(PerftTest, StartingPosition) {
       "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
   ChessBoard startingBoard(startingPositionFEN);
 
-  EXPECT_EQ(perft(3, startingBoard,
-                  startingBoard.getFlat(true) | startingBoard.getFlat(false)),
-            8902);
+  const PerftResult result = perft(4, startingBoard);
+
+  std::cout << "Nodes: " << result.nodes << '\n';
+  std::cout << "Captures: " << result.captures << '\n';
+  std::cout << "Promotions: " << result.promotions << '\n';
+  std::cout << "Checks: " << result.checks << '\n';
+
+  EXPECT_EQ(result.nodes, 197281);
+  EXPECT_EQ(result.captures, 1576);
+  EXPECT_EQ(result.promotions, 0);
+  EXPECT_EQ(result.checks, 469);
 }
