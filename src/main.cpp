4504334625fe97c73ba0d31a523b31bfc565c79e
P1X3R <p1x3r@proton.me>
2025-08-05 17:58:40 -0500

    Basic UCI loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,49 @@
+#include "board.h"
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-auto main() -> int { std::cout << "Hello world!\n"; }
+auto tokenize(std::string &input) -> std::vector<std::string> {
+  std::vector<std::string> tokens;
+  std::stringstream stream(input);
+  std::string token;
+
+  while (stream >> token) {
+    tokens.push_back(token);
+  }
+
+  return tokens;
+}
+
+class UCI {
+private:
+  ChessBoard board;
+
+public:
+  void loop() {
+    std::string input;
+
+    while (std::getline(std::cin, input)) {
+      std::vector<std::string> tokens = tokenize(input);
+
+      if (tokens.empty()) {
+        continue;
+      }
+
+      if (tokens[0] == "uci") {
+        std::cout << "id name Tanathos\n";
+        std::cout << "id author P1x3r\n";
+        std::cout << "uciok\n";
+      } else if (tokens[0] == "isready") {
+        std::cout << "readyok\n";
+      } else if (tokens[0] == "position") {
+      } else if (tokens[0] == "go") {
+      } else if (tokens[0] == "quit") {
+        break;
+      }
+    }
+  }
+};
+
+auto main() -> int {}
