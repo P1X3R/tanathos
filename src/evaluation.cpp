ac72dd68194e28331d54899dfd64ebfcd135d933
P1X3R <p1x3r@proton.me>
2025-08-12 19:52:20 -0500

    Add piece-square table to evaluation function

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -1,13 +1,33 @@
+#include "bitboard.h"
 #include "board.h"
+#include "psqt.h"
 #include "searching.h"
 #include <cstdint>
 
 auto ChessBoard::evaluate() const -> std::int32_t {
   std::int32_t score = 0;
 
+  // Iterate through all piece types
   for (std::uint32_t type = Piece::PAWN; type <= Piece::KING; type++) {
-    score += std::popcount(whites[type]) * PIECE_VALUES[type];
-    score -= std::popcount(blacks[type]) * PIECE_VALUES[type];
+
+    for (std::uint32_t square = 0; square < BOARD_AREA; square++) {
+
+      if ((whites[type] & (1ULL << square)) != 0) {
+        score += PIECE_VALUES[type];
+        score += PSQT[type][square];
+      }
+
+      const std::uint32_t flippedSquare = square ^ (BOARD_AREA - BOARD_LENGTH);
+
+      if ((blacks[type] & (1ULL << square)) != 0) {
+        score -= PIECE_VALUES[type];
+        score -= PSQT[type][flippedSquare];
+      }
+    }
+
+    // Evaluate black pieces
+    for (std::uint32_t square = 0; square < BOARD_AREA; square++) {
+    }
   }
 
   return score;
