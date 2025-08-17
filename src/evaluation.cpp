e24dd2f3fad1814b91ad0e0b38bbb3fb7e558f33
P1X3R <p1x3r@proton.me>
2025-08-17 15:13:23 -0500

    Fix: flip square for whites instead of blacks perspective for correct use of PSQT

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -11,17 +11,16 @@ auto ChessBoard::evaluate() const -> std::int32_t {
   for (std::uint32_t type = Piece::PAWN; type <= Piece::KING; type++) {
 
     for (std::uint32_t square = 0; square < BOARD_AREA; square++) {
+      const std::uint32_t flippedSquare = square ^ (BOARD_AREA - BOARD_LENGTH);
 
       if ((whites[type] & (1ULL << square)) != 0) {
         score += PIECE_VALUES[type];
-        score += PSQT[type][square];
+        score += PSQT[type][flippedSquare];
       }
-
-      const std::uint32_t flippedSquare = square ^ (BOARD_AREA - BOARD_LENGTH);
 
       if ((blacks[type] & (1ULL << square)) != 0) {
         score -= PIECE_VALUES[type];
-        score -= PSQT[type][flippedSquare];
+        score -= PSQT[type][square];
       }
     }
 
